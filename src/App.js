dcced238e8ffe8a5406ca9b91c266b5c555c093e
Benjamin Lin <blin@Benjamins-MacBook-Pro.local>
2023-08-30 13:16:32 -0700

    add code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { initSuperflow } from '@usesuperflow/client';
 import { useEffect } from 'react';
@@ -14,17 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Test out Superflow by highlighting this text.
       </header>
     </div>
   );
